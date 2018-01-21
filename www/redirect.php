f189513168e8e30ae99ff08b079e2cf3eb51274f
Steffen Köhler <steffen@koehlers.de>
2018-01-21 13:27:39 +0100

    initial code for http redirection

---
+++ www/redirect.php
@@ -0,0 +1,58 @@
+
+
+<?php
+// the dynDNS variables shall be
+// ip
+// username
+// password
+ 
+ 
+     if($_GET['password']=="meinpasswort")
+     {
+         $handle=fopen("link.html","w+");
+	 $myip=$_GET['ip'];
+         fputs($handle,'<html><body><a href="ftp://'.$myip.'">ftp://'.$myip.'</a><p>'.$myip.'<body><html>');
+         fclose($handle);
+     }
+
+
+
+$ini_array = parse_ini_file("sample.ini");
+print_r($ini_array);
+
+function write_php_ini($array, $file)
+{
+    $res = array();
+    foreach($array as $key => $val)
+    {
+        if(is_array($val))
+        {
+            $res[] = "[$key]";
+            foreach($val as $skey => $sval) $res[] = "$skey = ".(is_numeric($sval) ? $sval : '"'.$sval.'"');
+        }
+        else $res[] = "$key = ".(is_numeric($val) ? $val : '"'.$val.'"');
+    }
+    safefilerewrite($file, implode("\r\n", $res));
+}
+
+function safefilerewrite($fileName, $dataToSave)
+{    if ($fp = fopen($fileName, 'w'))
+    {
+        $startTime = microtime(TRUE);
+        do
+        {            $canWrite = flock($fp, LOCK_EX);
+           // If lock not obtained sleep for 0 - 100 milliseconds, to avoid collision and CPU load
+           if(!$canWrite) usleep(round(rand(0, 100)*1000));
+        } while ((!$canWrite)and((microtime(TRUE)-$startTime) < 5));
+
+        //file was locked so now we can store information
+        if ($canWrite)
+        {            fwrite($fp, $dataToSave);
+            flock($fp, LOCK_UN);
+        }
+        fclose($fp);
+    }
+
+}
+
+?>
